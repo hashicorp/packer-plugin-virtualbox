0d6e9177a7e1e9959a217b60cc3531e96e011e03
Anurag Sharma <anurag5sh@gmail.com>
2025-04-25 13:05:40 +0530

    clean up comments

--- builder/virtualbox/iso/step_get_vm_defaults.go
+++ builder/virtualbox/iso/step_get_vm_defaults.go
@@ -13,7 +13,6 @@ import (
 	"time"
 
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
-	// packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 	vboxcommon "github.com/hashicorp/packer-plugin-virtualbox/builder/virtualbox/common"
 )
 
